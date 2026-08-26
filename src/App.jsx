e9372343f066c7706b63cb38e7c0987db96a328d
Sufiyan Shahid Professional Programmer <sufiyanshahid144@gmail.com>
2026-08-26 20:47:05 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,122 @@
-import React, { useEffect, useState } from "react";
-import GalleryCard from "./components/GalleryCard";
-import axios from "axios";
-import Footer from "./components/Footer";
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
 
-const App = () => {
-  const [data, setData] = useState([]);
-  const [page, setPage] = useState(1);
-  const [limit, setLimit] = useState(8);
-
-
-  console.log("limit -->",limit);
-  
-  const getData = async () => {
-    try {
-      const result = await axios.get(
-        `https://picsum.photos/v2/list?page=${page}&limit=${limit}`,
-      );
-
-      console.log(result.data);
-      setData(result.data)
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getData();
-  }, [page,limit]);
-
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="flex justify-between flex-col h-screen bg-black ">
-      {/* header */}
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
 
-      <div className="bg-gray-700 shadow-2xl">
-        <h1 className="font-bold p-3 text-white text-2xl pl-5">Gallery App</h1>
-      </div>
+      <div className="ticks"></div>
 
-      {/* main body */}
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
 
-      <div className="cardParent p-5 flex gap-5 justify-around flex-wrap overflow-y-auto">
-        {/* gallery card*/}
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
 
-        {data.length > 0 ? (
-          data.map((item) => <GalleryCard data={item} />)
-        ) : (
-          <p className="text-white font-bold text-center">Loading...</p>
-        )}
-      </div>
-
-      {/* footer */}
-      <Footer setPage={setPage}  setLimit={setLimit}/>
-    </div>
-  );
-};
-
-export default App;
+export default App
